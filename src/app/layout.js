2dd7c534863c97d7c9989dc2a05efb2b94c46750
Shrikant Soni <shrikantsoni809@gmail.com>
2026-01-05 00:29:16 +0530

    fix error

--- src/app/layout.js
+++ src/app/layout.js
@@ -80,8 +80,14 @@ export default function RootLayout({ children }) {
 
 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5HPQH6V5"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5HPQH6V5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
 
         <ReduxProvider>
           {children}
